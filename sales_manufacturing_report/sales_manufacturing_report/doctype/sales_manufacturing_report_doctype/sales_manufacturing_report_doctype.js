46ca1f520d74b431d1f5dd30a4fb6b8eb0dd45dd
Mangesh <mangesh.bhuskute@atritechnocrat.com>
2019-11-05 04:53:58 -0500

    CR:Mangesh 1) Code moved to renamed Reports. Deleting unused files.

--- sales_manufacturing_report/sales_manufacturing_report/doctype/sales_manufacturing_report_doctype/sales_manufacturing_report_doctype.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2019, Atri Developers and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Sales Manufacturing Report Doctype', {
-	// refresh: function(frm) {
-
-	// }
-});
